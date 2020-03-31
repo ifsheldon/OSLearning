75d75114f092aaaa2a51ed3953ce6f15eb8fa7cf
Maple <maplevantablack@outlook.com>
2020-04-01 00:42:52 +0800

    change time

--- report3/lab4_code_for_report/sample.c
+++ report3/lab4_code_for_report/sample.c
@@ -3,6 +3,6 @@
 #include <time.h>
 int main()
 {
-    for(clock_t start=clock(); clock()-start< 101*1000; );
+    for(clock_t start=clock(); (double)((clock()-start)/CLOCKS_PER_SEC)< 2.1; );
     return 0;
 }
