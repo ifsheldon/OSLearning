78c96fce9926ba7c6b70653ab4f007e0b2236d58
Maple <maplevantablack@outlook.com>
2020-04-07 20:07:18 +0800

    finished hw but not checked

--- report4/reader-writer/read.h
+++ report4/reader-writer/read.h
@@ -1,14 +1,25 @@
 #include"init.h"
-
+//given two sem db, rc
 void *reader(int *buffer){
     // Reader should read the value of buffer
     // Reader cannot start to work when there is any writer working
     // You should implement this using semaphore
 
     // You should output this just after this thread gets semaphore
+    sem_wait(&rc);
     printf("Reader gets sem\n");
+    readcount++;
+    if(readcount==1) // for the first reader of successive readers, block writers
+        sem_wait(&db);     
+    sem_post(&rc);
 
+    sem_wait(&rc);
+    readcount--;
     // you should output this just before this thread releases the semaphore
-    printf("Reader reads %d and releases\n", *buffer); 
+    printf("Reader reads %d and releases\n", *buffer);
+    if(readcount==0)// for the first reader of successive readers, unblock writers
+        sem_post(&db);
+    
+    sem_post(&rc);
 }
 
--- report4/reader-writer/write.h
+++ report4/reader-writer/write.h
@@ -6,9 +6,11 @@ void *writer(int *buffer){
     // You should implement this using semaphore
 
     // You should output this just after this thread gets semaphore
+    sem_wait(&db);
     printf("writer gets sem\n");
-    
+    (*buffer)++;
     // you should output this just before this thread releases the semaphore
     printf("writer writes %d and releases\n", *buffer);
+    sem_post(&db);
 }
 
