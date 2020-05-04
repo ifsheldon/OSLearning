a358966ad5901437f9fd6401cc1c6a462289f6e2
Maple <maplevantablack@outlook.com>
2020-05-04 16:26:11 +0800

    finished clock, unchecked

--- report7/page_replacement/main.cpp
+++ report7/page_replacement/main.cpp
@@ -64,6 +64,7 @@ struct Node
     int val;
     Node *prev;
     Node *next;
+    bool valid;
 };
 
 inline void lru(const int *pageSequence, int length, int cacheSize)
@@ -196,16 +197,43 @@ inline void clock(const int *pageSequence, int length, int cacheSize)
     Node *nodes = new Node[cacheSize];
     nodes[0].prev = nullptr;
     nodes[0].next = nodes + 1;
+    nodes[0].valid = false;
     nodes[cacheSize - 1].next = nullptr;
     nodes[cacheSize - 1].prev = nodes + cacheSize - 2;
-    int usedSpace = 0;
+    nodes[cacheSize - 1].valid = false;
     int missCount = 0;
     for (int i = 1; i < cacheSize - 1; i++)
     {
         nodes[i].prev = nodes + (i - 1);
         nodes[i].next = nodes + (i + 1);
+        nodes[i].valid = false;
     }
-
+    Node *currentPointing = nodes;
+    for (int i = 0; i < length; i++)
+    {
+        int page = pageSequence[i];
+        auto found = cachedPages.find(page);
+        if (found != cachedPages.end())//hit
+        {
+            found->second->valid = true;
+        } else
+        {
+            missCount++;
+            if (cachedPages.size() < cacheSize)
+            {
+                (nodes + cachedPages.size())->valid = true;
+                cachedPages[page] = nodes + cachedPages.size();
+            } else
+            {
+                for (; currentPointing->valid; currentPointing->valid = false, currentPointing = currentPointing->next);
+                currentPointing->valid = true;
+                int previousPage = currentPointing->val;
+                cachedPages.erase(previousPage);
+                cachedPages[page] = currentPointing;
+            }
+        }
+    }
+    printResult(length, missCount);
     delete[] nodes;
 }
 
